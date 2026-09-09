08c1007b39f0c429578d31ea44f12e36318871e3
Chandni shukla <chandnishukla9770@gmail.com>
2026-09-09 17:51:23 +0530

    Allow Railway domain for CSRF

--- makemytrip/settings.py
+++ makemytrip/settings.py
@@ -162,8 +162,10 @@ CORS_ALLOWED_ORIGINS = [
     "https://make-my-trip-travel-booking-project-arje6t673-make-my-trip.vercel.app",
 ]
 
+
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://make-my-trip-travel-booking-project-arje6t673-make-my-trip.vercel.app",
+    "https://makemytrip-travel-booking-project-production.up.railway.app",
 ]
